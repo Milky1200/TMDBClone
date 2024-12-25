0868c7ca2e9796e7b03320f5e318259ba0cc7527
mishraaman435 <mishraaman435@gmail.com>
2024-12-25 15:42:56 +0530

    The App is Ready with the Basic Functionality

--- app/src/main/java/com/example/tmdb/MainActivity.java
+++ app/src/main/java/com/example/tmdb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tmdb;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toolbar;
@@ -9,7 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.tmdb.adapter.MovieAdapter;
 import com.example.tmdb.model.Movie;
 import com.example.tmdb.model.MovieDBResponse;
 import com.example.tmdb.service.MovieDataService;
@@ -23,7 +29,10 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Movie> movies;
-private static final String APIKEY="24381b07507d61664234bfa4f4ae29b0";
+    RecyclerView recyclerView;
+    MovieAdapter movieAdapter;
+    SwipeRefreshLayout swipeRefreshLayout;
+//private static final String APIKEY="24381b07507d61664234bfa4f4ae29b0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,26 +44,48 @@ private static final String APIKEY="24381b07507d61664234bfa4f4ae29b0";
             return insets;
         });
 
+        swipeRefreshLayout=findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.setColorSchemeColors(R.color.background);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getPopularMovies();
+            }
+        });
         getPopularMovies();
+
     }
 
     public void getPopularMovies() {
         MovieDataService service= RetrofitInstance.getService();
-        Call<MovieDBResponse> responseCall=service.getPopularMovies(APIKEY);
+        Call<MovieDBResponse> responseCall=service.getPopularMovies(getString(R.string.api_key));
         responseCall.enqueue(new Callback<MovieDBResponse>() {
             @Override
             public void onResponse(Call<MovieDBResponse> call, Response<MovieDBResponse> response) {
                 MovieDBResponse movieDBResponse=response.body();
                 if(movieDBResponse!=null && movieDBResponse.getResults()!=null){
                     movies=(ArrayList<Movie>) movieDBResponse.getResults();
+                    showOnRecyclerView();
 
                 }
             }
 
             @Override
             public void onFailure(Call<MovieDBResponse> call, Throwable t) {
-                Log.e("Failed", "onFailure: No response" );
+                Log.e("Failed", "onFailure: No response : "+t.getMessage());
             }
         });
+    }
+
+    private void showOnRecyclerView() {
+        recyclerView=findViewById(R.id.rvMovies);
+        movieAdapter=new MovieAdapter(this,movies);
+        if(this.getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        }else recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.notifyDataSetChanged();
     }
 }
